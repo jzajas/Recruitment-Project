a003792c55889547efb558de7a56a6d6ca892676
jzajas <jzajas1@gmail.com>
2026-01-09 17:15:05 +0100

    initial commit

---
+++ src/test/java/com/jzajas/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzajas.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
