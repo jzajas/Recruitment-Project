aaf91893ddc6b990f3d1afde71acc43265795090
jzajas <jzajas1@gmail.com>
2026-01-12 18:23:13 +0100

    backend url change

--- backend/src/main/java/com/jzajas/task/config/CorsConfig.java
+++ backend/src/main/java/com/jzajas/task/config/CorsConfig.java
@@ -15,10 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://frontend-production-e47a.up.railway.app"
-                )
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
